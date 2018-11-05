a523221e0de1293c38b6d3d43e6290dfcc864e6e
Ryan Kang <shk130@ucsd.edu>
2018-11-04 21:27:58 -0800

    added new webpack

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './App.css';
+
+export default class App extends Component {
+    render () {
+        return <div id="background">
+            <div id="header">
+                <h1>Howdy partner</h1>
+            </div>  
+            <input></input>
+        </div>
+    }
+}
