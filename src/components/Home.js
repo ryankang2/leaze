eb61fb38cd4cabc2c2a5b60f9b80b8e4002ca00b
aharmlesskitty <khc033@ucsd.edu>
2018-11-08 15:19:53 -0800

    Started Home.js page

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "./App.css";
+
+export default class Home extends Component {
+    render () {
+        return <div id="background">
+            <div id="header">
+                <h1>Howdy partner</h1>
+                <p>Gary hello</p>
+            </div>  
+            <input></input>
+        </div>
+    }
+}
