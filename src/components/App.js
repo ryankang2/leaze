9ffda709ccb9e9fcd4b6237f9587a0051cdcdbee
abtsai <abtsai@ucsd.edu>
2018-11-11 23:20:53 -0800

    what the fucl

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,21 @@ import "./App.css";
 
 export default class App extends Component {
     render () {
-        return <div id="background">
+        return (<div id="background">
             <div id="header">
-                <h1>Howdy partner</h1>
-                <p>Gary is a ball liker</p>
-            </div>  
-            <input></input>
+                <h1>LEAZE</h1>
+                <div className="topnav">
+                    <input type="text" placeholder="Search.."></input>
+                    <a className="active" href="#home">Home</a>
+                    <a href="#????????">????????</a>
+                    <a href="#messages">Messages</a>
+                    <a href="#notifications">Notifications</a>
+
+                </div>
+
+            </div>
+
         </div>
+        );
     }
 }
