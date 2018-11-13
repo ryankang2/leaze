c22143db00a14b32b2b0855bedcaa14e90cb7f72
Ryan Kang <shk130@ucsd.edu>
2018-11-12 17:09:13 -0800

    added picture of cat

--- src/components/ListingPreview.js
+++ src/components/ListingPreview.js
@@ -20,7 +20,12 @@ class ListingPreview extends Component{
         return (
             <div class="list">
                 <div className="row">
-                    <div className="col-sm-4 singleListing">Hi</div>
+                    <div className="col-sm-4 singleListing">Hi
+                        <div>
+                            <img src="https://carwad.net/sites/default/files/styles/225x120/public/cat-images-free-122692-172264.jpg?itok=lq443nq_" alt=""/>
+
+                        </div>
+                    </div>
                     <div className="col-sm-4 singleListing">Hello</div>   
                     <div className="col-sm-4 singleListing">frick yeah</div>                
                 </div>
