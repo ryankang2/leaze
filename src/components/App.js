62a32b4d5bef65202077f24c8d03516fa0f22775
Ryan Kang <shk130@ucsd.edu>
2018-11-12 16:44:29 -0800

    made container

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,25 @@ import React, {Component} from "react";
 import ResultsPage from "./ResultsPage";
 import "./App.css";
 
-export default class App extends Component {
-    render () {
-        return <div id="background">
-            <div id="header">
-                <h1>Howdy partner</h1>
-                <p>Gary is a ball liker</p>
-            </div>  
-            <input></input>
-        </div>
-    }
-}
-
 // export default class App extends Component {
 //     render () {
-//         return (
-//             <div>
-//                 <ResultsPage />
-//             </div>
-//         )
+//         return <div id="background">
+//             <div id="header">
+//                 <h1>Howdy partner</h1>
+//                 <p>Gary is a ball liker</p>
+//             </div>  
+//             <input></input>
+//         </div>
 //     }
 // }
+
+export default class App extends Component {
+    render () {
+        return (
+            <div>
+                <ResultsPage />
+            </div>
+        )
+    }
+}
 
