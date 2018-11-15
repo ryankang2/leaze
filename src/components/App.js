dd585040d858e3069a852d592ce9870103867c83
drexler <djk008@ucsd.edu>
2018-11-15 14:54:07 -0800

    stuff

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,8 @@ import "./App.css";
 
 export default class App extends Component {
     render () {
-        return <div id="background">
-            <div id="header">
-                <h1>Howdy partner</h1>
-                <p>Gary is a ball liker</p>
-            </div>  
-            <input></input>
+        return <div>
+            <ResultsPage />
         </div>
     }
 }
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,8 +1,8 @@
 import React, {Component} from "react";
-import "./App.css";
-import "./person.js"
+import "./profile.css";
+//import "./update.js"
 
-export default class App extends Component {
+class Profile extends Component {
     constructor(props){
         super(props);
         this.state={
@@ -16,22 +16,70 @@ export default class App extends Component {
         };
     }
     render () {
-        return <div id="background">
+        return (
+        <div>
+    <div class="container-fluid" id="mainContainer">
+        {/* here lies the top row - Matt works here */}
+        <div id="background">
             <div id="header">
-                <h1>{this.state.name}'s Profile
-                    <img src="C:/Users/drexl/IdeaProjects/leaze/src/components/profile.jpg" width="100" height="100"></img>
-                </h1>
             </div>
+            <h1>{this.state.name}USER's Profile</h1>
             <div>
+              <img src={ require('./profile.jpg')} id="profilePictureArea" />
+            </div>
+            <div id="info">
                 <p>Sex: {this.state.sex}</p>
                 <p>Age: {this.state.age}</p>
                 <p>Hometown: {this.state.hometown}</p>
                 <p>Current School: {this.state.name}</p>
-                <p>Major: {this.state.major}</p>
+                <p> Major: {this.state.major}</p>
                 <p>Hobbies: {this.state.hobbies}</p>
+                <div class="col-md-4" id="bioArea">
+                  Bio:
+                  {/* User Bio Area here  */}
+                  <p rows = "4" cols="50">
+                    {this.state.bio} 
+                  </p>
+                </div>
+              <a href="update.js" float="left">Edit</a>
             </div>  
-            <br />
-            <button>Edit</button>
         </div>
+        <div class="row" id="profileRow">
+          <div class="col-md-2" id="profilePictureArea">
+            {/* User profile pic here */}
+          </div>
+
+
+          <div className="col-md-2" id="ratingArea">
+            {/* User Rating Area goes here */}
+          </div>
+
+          <div className="col-md-4" id="progressArea">
+            {/* User Profile Strength Percentage is here */}
+          </div>
+
+        </div>
+
+        <div class="row" id="rowDivider">
+          <hr /> {/* This is the pretty row divider */}
+        </div>
+
+        {/* here lies the bottom row - Drexler works here */}
+        <div class="row" id="listingsRow">
+          <div class="col-md-12">
+            <div id="postedListings">
+              {/* User's Posted Listings go here */}
+            </div>
+            <div id="favoriteListings">
+              {/* User's Favorite Listings go here */}
+              <img src={ require('./house.jpg')} id="favoriteListings" />
+            </div>
+          </div>
+        </div>
+    </div>
+    </div>
+        );
     }
 }
+
+export default Profile;
