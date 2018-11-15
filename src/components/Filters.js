b94ed28726f0f0a64b763ba1630cf932ad7ad8b8
aharmlesskitty <khc033@ucsd.edu>
2018-11-15 11:33:58 -0800

    Added checkboxes

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -25,18 +25,18 @@ class Filters extends Component{
                 <div className="listOfFilters">
                     <li className="roomType"><a href="#">Room Type</a>
                         <ul className="dropdownContainer">
-                            <li>Single Room</li>
-                            <li>Double Room</li>
-                            <li>Triple Room</li>
-                            <li>Living Room</li>
+                            <li><input type="checkbox" />Single Room</li>
+                            <li><input type="checkbox" />Double Room</li>
+                            <li><input type="checkbox" />Triple Room</li>
+                            <li><input type="checkbox" />Living Room</li>
                         </ul>
                     </li>
                     <li className="distance"><a>Distance Within</a>
                         <ul className="dropdownContainer">
-                            <li>2 mi</li>
-                            <li>5 mi</li>
-                            <li>10 mi</li>
-                            <li>20+ mi</li>
+                            <li><input type="checkbox" />2 mi</li>
+                            <li><input type="checkbox" />5 mi</li>
+                            <li><input type="checkbox" />10 mi</li>
+                            <li><input type="checkbox" />20+ mi</li>
                         </ul>
                     </li>
 
@@ -45,54 +45,6 @@ class Filters extends Component{
         )
     };
 }
-
-// class Item extends React.Component {
-//     constructor (props){
-//       super ();
-  
-//       this.state = {
-//         checked: false
-//       };
-  
-//       this.handleClick = this.handleClick.bind(this);    
-//     }
-//     handleClick (e){
-//       this.setState({
-//         checked: !this.state.checked
-//       });
-  
-//     }
-//     render (){
-//       let text = this.state.checked ? <strike>{this.props.message}</strike> : this.props.message;
-//       return (
-//           <div className="row">
-//             <div className="col-md-12">
-//               <input type="checkbox" onClick={this.handleClick} />&nbsp;{text}
-//               <hr />
-//             </div>
-//           </div>
-//       );
-//     }
-//   }
-  
-//   let item2 = <Item message="Single Room" />;
-//   let item3 = <Item message="Double Room" />;
-//   let item4 = <Item message="Triple Room" />;
-  
-  
-//   let allTheThings = [item2, item3, item4];
-  
-//   class ItemList extends React.Component {
-//     constructor (props){
-//       super ();
-//     }
-//     render (){
-//       let items = allTheThings.map(thing => thing);
-//       return (
-//           <h4>{items}</h4>
-//       );
-//     }
-//   }
 
 
 export default Filters;
