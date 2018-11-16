0699991eb1a51e7e5ef05f7e19cbd9d71fe49b73
tesriram <43828762+tesriram@users.noreply.github.com>
2018-11-16 15:43:42 -0800

    For backend testing. Landing page is very nearly complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from "react";
-import ResultsPage from "./ResultsPage";
-import "./App.css";
+// import ResultsPage from "./ResultsPage";
 import LandingPage from "./LandingPage";
 
 export default class App extends Component {
     render () {
         return (
             <div>
-                <ResultsPage />
+                {/* <ResultsPage /> */}
+                <LandingPage />
             </div>
         )
     }
