104662c402d8cd3a466205f67909dcf53b199aec
Ryan Kang <shk130@ucsd.edu>
2018-11-16 15:44:05 -0800

    added phpmailer files

---
+++ public/api/mysql_connect.php
@@ -0,0 +1,4 @@
+<?php
+    $conn = mysqli_connect("54.183.156.193", "localhost", "goldenhong", "leaze");
+
+?>
--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ import App from "./components/App.js";
 import Profile from "./components/profile.js";
 import Update from "./components/update.js";
 
-ReactDOM.render(<Profile/>, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
 
