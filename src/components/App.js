5aa07960e3341e62e713a4240e2ab1d83696bb35
Ryan Kang <shk130@ucsd.edu>
2018-11-18 15:39:28 -0800

    fixed indexjs

--- src/components/App.js
+++ src/components/App.js
@@ -12,8 +12,8 @@ export default class App extends Component {
     render () {
         return (
             <div>
-                {/* <ResultsPage /> */}
-                <LandingPage />
+                <ResultsPage />
+                {/* <LandingPage /> */}
             </div>
         )
     }
