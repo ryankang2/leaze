020df0c9e7dc58605a7df7d18d1e819e59885863
aharmlesskitty <khc033@ucsd.edu>
2018-11-17 19:23:41 -0800

    Wrote the array for the checkboxes in FilterItems.js.

---
+++ src/components/FilterItems.js
@@ -0,0 +1,44 @@
+const roomTypeList = [
+    {
+        name: 'single-room',
+        key: 'singleRoom',
+        label: 'Single Room',
+    },
+    {
+        name: 'double-room',
+        key: 'doubleRoom',
+        label: 'Double Room',
+    },
+    {
+        name: 'triple-room',
+        key: 'tripleRoom',
+        label: 'Triple Room',
+    },
+    {
+        name: 'living-room',
+        key: 'livingRoom',
+        label: 'Living Room',
+    },
+];
+const distanceList = [
+    {
+        name: '2-miles',
+        key: '2Miles',
+        label: '2 Miles',
+    },
+    {
+        name: '5-miles',
+        key: '5Miles',
+        label: '5 Miles',
+    },
+    {
+        name: '10-miles',
+        key: '10Miles',
+        label: '10 Miles',
+    },
+    {
+        name: '20-plus-miles',
+        key: '20PlusMiles',
+        label: '20+ Miles',
+    },
+];
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,24 +1,36 @@
 import React, {Component} from "react";
-import "./Filters.css"
+import "./Filters.css";
+import {roomTypeList, distanceList} from './FilterItems';
 
 class Filters extends Component{
     constructor(props){
         super(props);
         this.state = {
-            singleRoom: true,
-            doubleRoom: true,
-            tripleRoom: true,
-            livingRoom: true,
+            singleRoom: false,
+            doubleRoom: false,
+            tripleRoom: false,
+            livingRoom: false,
+            mi2: false,
+            mi5: false,
+            mi10: false,
+            mi20plus: false,
         }
 
     }
     resetFilters(){
         console.log("reset");
     }
-    dropdownFilter(e) {
+    listClicked(e) {
         e.preventDefault();
 
+        console.log('You clicked the list header');
+    }
 
+    handleClick(e) {
+        this.setState(state => ({
+
+        }))
+        console.log('Clicked checkbox');
     }
     render(){
         return (
@@ -28,20 +40,24 @@ class Filters extends Component{
                     <button className="btn btn-link" onClick={this.resetFilters}>Clear All</button>
                 </div>
                 <div className="listOfFilters">
-                    <li className="roomType"><a href="#">Room Type</a>
+                    <li className="roomType" onClick={this.listClicked}><a href="#">Room Type</a>
                         <ul className="dropdownContainer">
-                            <li><input type="checkbox" />Single Room</li>
-                            <li><input type="checkbox" />Double Room</li>
-                            <li><input type="checkbox" />Triple Room</li>
-                            <li><input type="checkbox" />Living Room</li>
+                            <li><input 
+                                    type="checkbox" 
+                                    checked={this.state.singleRoom}
+                                    onClick={this.handleClick} 
+                                />Single Room</li>
+                            <li><input type="checkbox" checked={this.state.doubleRoom} />Double Room</li>
+                            <li><input type="checkbox" checked={this.state.tripleRoom} />Triple Room</li>
+                            <li><input type="checkbox" checked={this.state.livingRoom} />Living Room</li>
                         </ul>
                     </li>
-                    <li className="distance"><a>Distance Within</a>
+                    <li className="distance" onClick={this.listClicked} ><a href="#">Distance Within</a>
                         <ul className="dropdownContainer">
-                            <li><input type="checkbox" />2 mi</li>
-                            <li><input type="checkbox" />5 mi</li>
-                            <li><input type="checkbox" />10 mi</li>
-                            <li><input type="checkbox" />20+ mi</li>
+                            <li><input type="checkbox" onClick={this.handleChecked} />2 mi</li>
+                            <li><input type="checkbox" onClick={this.handleChecked} />5 mi</li>
+                            <li><input type="checkbox" onClick={this.handleChecked} />10 mi</li>
+                            <li><input type="checkbox" onClick={this.handleChecked} />20+ mi</li>
                         </ul>
                     </li>
 
