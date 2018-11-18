310ae543cb890eb7e4acda488b84fd627853c5bb
aharmlesskitty <khc033@ucsd.edu>
2018-11-17 23:22:32 -0800

    Edited names to have correct spelling.

--- src/components/FilterItems.js
+++ src/components/FilterItems.js
@@ -1,43 +1,43 @@
 const roomTypeList = [
     {
-        name: 'single-room',
+        name: 'Single room',
         key: 'singleRoom',
         label: 'Single Room',
     },
     {
-        name: 'double-room',
+        name: 'Double room',
         key: 'doubleRoom',
         label: 'Double Room',
     },
     {
-        name: 'triple-room',
+        name: 'Triple room',
         key: 'tripleRoom',
         label: 'Triple Room',
     },
     {
-        name: 'living-room',
+        name: 'Living room',
         key: 'livingRoom',
         label: 'Living Room',
     },
 ];
 const distanceList = [
     {
-        name: '2-miles',
+        name: '2 miles',
         key: '2Miles',
         label: '2 Miles',
     },
     {
-        name: '5-miles',
+        name: '5 miles',
         key: '5Miles',
         label: '5 Miles',
     },
     {
-        name: '10-miles',
+        name: '10 miles',
         key: '10Miles',
         label: '10 Miles',
     },
     {
-        name: '20-plus-miles',
+        name: '20+ miles',
         key: '20PlusMiles',
         label: '20+ Miles',
     },
