3b079b5d87b18a1eb4ae7fce37816621f90fec92
abtsai <abtsai@ucsd.edu>
2018-11-17 16:29:43 -0800

    need to push

--- src/components/ListingPreview.js
+++ src/components/ListingPreview.js
@@ -20,16 +20,75 @@ class ListingPreview extends Component{
         return (
             <div class="list">
                 <div className="row">
-                    <div className="col-sm-4 singleListing">Hi
-                        <div>
 
+                    <div className="col-sm-4 singleListing">
+                        <div className = "col-sm-4 imageBox">
+                            <img className="leaseImage" src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7A4jeJ_RBCBZL7kHIc9CSDn3XdSfWgHBOJ1L2ieqBvx9eLcubrQ"/>
+                            <div className= "leaseName">HAHAH HI TITLE</div>
+                        </div>
+                        <div className = "col-sm-4 infoBox">
+                            <img className="userPicture"
+                                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7A4jeJ_RBCBZL7kHIc9CSDn3XdSfWgHBOJ1L2ieqBvx9eLcubrQ"/>
+                            <div className="userName"> ARIBERRy:P</div>
+                            <div className="distance"> 0.69MI</div>
+                            <div className="userRating">
+                                <span className="fa fa-star checked"></span>
+                                <span className="fa fa-star checked"></span>
+                                <span className="fa fa-star checked"></span>
+                                <span className="fa fa-star"></span>
+                                <span className="fa fa-star"></span>
+
+                            </div>
+                            <div className="dateDiff"> 1 Day Ag0</div>
                         </div>
                     </div>
-                    <div className="col-sm-4 singleListing">Hello</div>   
-                    <div className="col-sm-4 singleListing">frick yeah</div>                
+                    <div className="col-sm-4 singleListing">
+                        <div className = "col-sm-4 imageBox">
+                            <img className="leaseImage" src = "https://www.rd.com/wp-content/uploads/2018/02/30_Adorable-Puppy-Pictures-that-Will-Make-You-Melt_124167640_YamabikaY-760x506.jpg"/>
+                                <div className= "leaseName">HAHAH HI TITLE</div>
+                        </div>
+                        <div className = "col-sm-4 infoBox">
+                            <img className="userPicture"
+                                 src="https://www.rd.com/wp-content/uploads/2018/02/30_Adorable-Puppy-Pictures-that-Will-Make-You-Melt_124167640_YamabikaY-760x506.jpg"/>
+                            <div className="userName"> ARIBERRy:P</div>
+                            <div className="distance"> 0.69MI</div>
+                            <div className="userRating"> 10/10</div>
+                            <div className="dateDiff"> 1 Day Ag0</div>
+
+                        </div>
+
+                    </div>
+
+                    <div className="col-sm-4 singleListing">
+                        <div className = "col-sm-4 imageBox">
+                            <img className="leaseImage" src = "http://images6.fanpop.com/image/photos/41500000/adorable-puppies-cute-puppies-41538743-590-393.jpg"/>
+                                <div className= "leaseName">HAHAH HI TITLE</div>
+                        </div>
+                        <div className = "col-sm-4 infoBox">
+                            <img className="userPicture"
+                                 src="http://images6.fanpop.com/image/photos/41500000/adorable-puppies-cute-puppies-41538743-590-393.jpg"/>
+                            <div className="userName"> ARIBERRy:P</div>
+                            <div className="distance"> 0.69MI</div>
+                            <div className="userRating"> 10/10</div>
+                            <div className="dateDiff"> 1 Day Ag0</div>
+                        </div>
+                    </div>
                 </div>
                 <div className="row">
-                    <div className="col-sm-4 singleListing">Hi</div>             
+                    <div className="col-sm-4 singleListing">
+                        <div className = "col-sm-4 imageBox">
+                            <img className="leaseImage" src = "https://2static.fjcdn.com/large/pictures/c3/1e/c31e9d_5929666.jpg"/>
+                            <div className= "leaseName">HAHAH HI TITLE</div>
+                        </div>
+                        <div className = "col-sm-4 infoBox">
+                            <img className="userPicture"
+                                 src="https://2static.fjcdn.com/large/pictures/c3/1e/c31e9d_5929666.jpg"/>
+                            <div className="userName"> ARIBERRy:P</div>
+                            <div className="distance"> 0.69MI</div>
+                            <div className="userRating"> 10/10</div>
+                            <div className="dateDiff"> 1 Day Ag0</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
