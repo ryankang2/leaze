87b3ceabc95f2580c23e3f9e25ebd87177b44f1f
tesriram <43828762+tesriram@users.noreply.github.com>
2018-11-26 18:16:07 -0800

    finished landing page except for input error messages

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, {Component} from "react";
 // import ResultsPage from "./ResultsPage";
 import LandingPage from "./LandingPage";
+import MessagingPage from "./MessagingPage";
 
 export default class App extends Component {
     render () {
         return (
             <div>
                 {/* <ResultsPage /> */}
-                <LandingPage />
+                {/* <LandingPage /> */}
+                <MessagingPage />
             </div>
         )
     }
