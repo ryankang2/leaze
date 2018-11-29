24f8090c4e6403407a370e088882252df75556fa
Eric Liu <eric.sa.liu@gmail.com>
2018-11-29 15:23:51 -0800

    edits on preferences

--- public/api/queries/get_pref.php
+++ public/api/queries/get_pref.php
@@ -3,29 +3,30 @@
     header("Access-Control-Allow-Headers: *");
     require_once("../mysql_connect.php");
     $output = [
-        "success" = false;
-        "guests_per_week" = -1;
-        "late_sleeper" = -1;
-        "deep_sleeper" = -1;
-        "early_bird" = -1;
-        "messiness" = -1;
-        "video_games" = -1;
-        "extrovert" = -1;
-        "study_noise_level" = -1;
-        "sharing_belongings" = -1;
-        "roommate_relationship" = -1;
-        "substances" = -1;
+        "success" => false,
+        "guests_per_week" => 0,
+        "late_sleeper" => 0,
+        "deep_sleeper" => 0,
+        "early_bird" => 0,
+        "messiness" => 0,
+        "video_games" => 0,
+        "extrovert" => 0,
+        "study_noise_level" => 0,
+        "sharing_belongings" => 0,
+        "roommate_relationship" => 0,
+        "substances" => 0
     ];
 
     //assume we can pass in the user id, and we output the information in output
-    $user = $_POST["user_id"];
+    //$user = $_POST["user_id"];
+    $user = 6;
 
-    $prefQuery= "SELECT * from `preferences` WHERE `preferences_id`='$user'";
-    $result = mysqli_query($conn, $checkUserQuery);
+    $prefQuery= "SELECT * from `preferences` WHERE `user_id`='$user'";
+    $result = mysqli_query($conn, $prefQuery);
     
     // should only return one row (make sure to delete upon user 
-    if(mysqli_num_rows($result) == 1){
-        $row = mysqli_fetch_assoc($result)
+    if(mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
         $output["guests_per_week"] = $row["guests_per_week"];
         $output["late_sleeper"] = $row["late_sleeper"];
         $output["deep_sleeper"] = $row["deep_sleeper"];
@@ -39,9 +40,7 @@
         $output["substances"] = $row["substances"];
         $ourput["success"] = true;
     }
-
     mysqli_close($conn);
     print_r(json_encode($output));
-
 
 ?>
