3b9519ad2b4e2b04e0b48f05af9dbd64bd3c1ec3
abtsai <abtsai@ucsd.edu>
2018-11-30 02:35:33 -0800

    workin on modal

--- src/components/ListingPreview.js
+++ src/components/ListingPreview.js
@@ -20,7 +20,8 @@ class ListingPreview extends Component{
     }
 
     leasePopup(){
-        console.log("PICTURE CLICKED");
+        console.log("PICTURE/CLICKED");
+
 
     }
 
@@ -61,8 +62,7 @@ class ListingPreview extends Component{
         var linkQuery = "/profile/" + this.props.information.user.user_id;
 
         return (
-            // <div className="list">
-            //     <div className="row">
+                <div>
                     <div className="col-sm-4 singleListing">
                         <div className="imageBox">
                             <img className="leaseImage" onClick={ this.leasePopup} src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7A4jeJ_RBCBZL7kHIc9CSDn3XdSfWgHBOJ1L2ieqBvx9eLcubrQ"/>
@@ -70,7 +70,7 @@ class ListingPreview extends Component{
                             <i className="fa fa-heart-o favorite" onClick={(event) => this.toggleHeart(event)}> </i>
 
                             <div className = "infoBox">
-                                <div className="leaseName"  onClick={ this.leasePopup}>
+                                <div className="leaseName"  data-toggle="modal" onClick={ this.leasePopup} data-target="#popupModal">
                                     {title}
                                 </div>
 
@@ -88,116 +88,32 @@ class ListingPreview extends Component{
                                 <div className="address"> {address}</div>
                                 <div className="price"> ${price}/month</div>
 
-                                {/*<div className="userRating">*/}
-                                {/*<i className="fa fa-star"></i>*/}
-                                {/*<i className="fa fa-star"></i>*/}
-                                {/*<i className="fa fa-star"></i>*/}
-                                {/*<i className="fa fa-star-o"></i>*/}
-                                {/*<i className="fa fa-star-o"></i>*/}
-                                {/*</div>*/}
                                 {/*<div className="dateDiff">{this.getDiffPostDate(day, month, listing_day, listing_month)}</div>*/}
                             </div>
                         </div>
                     </div>
-            //     </div>
-            // </div>
-            // <div class="list">
-            //     <div className="row">
+                    <div id="popupModal" className="modal">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
 
-            //         <div className="col-sm-4 singleListing">
-            //             <div className = "imageBox">
-            //                 <img className="leaseImage" onClick={ this.enlargePicture} src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7A4jeJ_RBCBZL7kHIc9CSDn3XdSfWgHBOJ1L2ieqBvx9eLcubrQ"/>
-            //                 <i className="fa fa-star-o favorite"></i>
-            //                 <div className= "leaseName">HAHAH HI TITLE</div>
-            //             </div>
-            //             <div className = "infoBox">
-            //                 <img className="userPicture" onClick={(event) => this.goToProfile(event)}
-            //                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7A4jeJ_RBCBZL7kHIc9CSDn3XdSfWgHBOJ1L2ieqBvx9eLcubrQ"/>
-            //                 <div className="userName" onClick={(event) => this.goToProfile(event)}> ARIBERRy:P</div>
-            //                 <div className="distance"> 0.69MI</div>
-            //                 <div className="userRating">
-            //                    <i className="fa fa-star"></i>
-            //                    <i className="fa fa-star"></i>
-            //                    <i className="fa fa-star"></i>
-            //                    <i className="fa fa-star-o"></i>
-            //                    <i className="fa fa-star-o"></i>
-            //                 </div>
-            //                 <div className="dateDiff"> 1 Day Ag0</div>
-            //             </div>
-            //         </div>
-            //         <div className="col-sm-4 singleListing">
-            //             <div className = "imageBox">
-            //                 <img className="leaseImage" src = "https://www.rd.com/wp-content/uploads/2018/02/30_Adorable-Puppy-Pictures-that-Will-Make-You-Melt_124167640_YamabikaY-760x506.jpg"/>
-            //                 <i className="fa fa-star-o favorite"></i>
-            //                 <div className= "leaseName">HAHAH HI TITLE</div>
+                                <div className="modal-header">
+                                    <h4 className="modal-title">Modal Heading</h4>
+                                    <button type="button" className="close" data-dismiss="modal">&times;</button>
+                                </div>
 
-            //             </div>
-            //             <div className = "infoBox">
-            //                 <img className="userPicture"
-            //                      src="https://www.rd.com/wp-content/uploads/2018/02/30_Adorable-Puppy-Pictures-that-Will-Make-You-Melt_124167640_YamabikaY-760x506.jpg"/>
-            //                 <div className="userName"> ARIBERRy:P</div>
-            //                 <div className="distance"> 0.69MI</div>
-            //                 <div className="userRating">
-            //                     <i className="fa fa-star"></i>
-            //                     <i className="fa fa-star"></i>
-            //                     <i className="fa fa-star"></i>
-            //                     <i className="fa fa-star-o"></i>
-            //                     <i className="fa fa-star-o"></i>
+                                <div className="modal-body">
+                                    Modal body..
+                                </div>
 
-            //                 </div>
-            //                 <div className="dateDiff"> 1 Day Ag0</div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-danger" data-dismiss="modal">Close
+                                    </button>
+                                </div>
 
-            //             </div>
-
-            //         </div>
-
-            //         <div className="col-sm-4 singleListing">
-            //             <div className = "imageBox">
-            //                 <img className="leaseImage" src = "http://images6.fanpop.com/image/photos/41500000/adorable-puppies-cute-puppies-41538743-590-393.jpg"/>
-            //                 <i className="fa fa-star-o favorite"></i>
-            //                 <div className= "leaseName">HAHAH HI TITLE</div>
-            //             </div>
-            //             <div className = "infoBox">
-            //                 <img className="userPicture"
-            //                      src="http://images6.fanpop.com/image/photos/41500000/adorable-puppies-cute-puppies-41538743-590-393.jpg"/>
-            //                 <div className="userName"> ARIBERRy:P</div>
-            //                 <div className="distance"> 0.69MI</div>
-            //                 <div className="userRating">
-            //                     <i className="fa fa-star"></i>
-            //                     <i className="fa fa-star"></i>
-            //                     <i className="fa fa-star"></i>
-            //                     <i className="fa fa-star-o"></i>
-            //                     <i className="fa fa-star-o"></i>
-            //                 </div>
-            //                 <div className="dateDiff"> 1 Day Ag0</div>
-            //             </div>
-            //         </div>
-            //     </div>
-            //     <div className="row">
-            //         <div className="col-sm-4 singleListing">
-            //             <div className = "imageBox">
-            //                 <img className="leaseImage" src = "https://2static.fjcdn.com/large/pictures/c3/1e/c31e9d_5929666.jpg"/>
-            //                 <i className="fa fa-star-o favorite"></i>
-            //                 <div className= "leaseName">HAHAH HI TITLE</div>
-            //             </div>
-            //             <div className = "infoBox">
-            //                 <img className="userPicture"
-            //                      src="https://2static.fjcdn.com/large/pictures/c3/1e/c31e9d_5929666.jpg"/>
-            //                 <div className="userName"> ARIBERRy:P</div>
-            //                 <div className="distance"> 0.69MI</div>
-            //                 <div className="userRating">
-            //                     <i className="fa fa-star"></i>
-            //                     <i className="fa fa-star"></i>
-            //                     <i className="fa fa-star"></i>
-            //                     <i className="fa fa-star-o"></i>
-            //                     <i className="fa fa-star-o"></i>
-
-            //                 </div>
-            //                 <div className="dateDiff"> 1 Day Ag0</div>
-            //             </div>
-            //         </div>
-            //     </div>
-            // </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         );
     }
 
