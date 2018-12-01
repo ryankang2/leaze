bba7379d8f949245d7cf91632cbc684d01bb6862
Ryan Kang <shk130@ucsd.edu>
2018-11-30 23:08:17 -0800

    ready

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ const App = () => (
                 {/* <Switch>
                     <Route exact path="/" component={LandingPage}></Route>
                     <Route exact path="/home" component={ResultsPage}></Route>
+                    <Route exact path="/home/profile/:number" component={Profile}></Route>
                 </Switch> */}
                 {/* <ResultsPage />  */}
                 {/* <LandingPage /> */}
