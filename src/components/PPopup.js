26c0a34e496b81b971a220250a6e3454950a6471
drexler <djk008@ucsd.edu>
2018-12-02 14:25:10 -0800

    added no preference option

--- src/components/PPopup.js
+++ src/components/PPopup.js
@@ -92,6 +92,7 @@ class PPopup extends React.Component {
             <label>How many guests days per week?: </label>
             <Input s={12} id='guests' className="inputs" type='select' name='gpd' value={this.state.gpd}
               onChange={this.handleChange.bind(this)} >
+              <option value='0'>No Pref</option> 
               <option value='-1'>0 Days</option>
               <option value='1'>1 Day</option>
               <option value='2'>2 Days</option>
@@ -104,18 +105,21 @@ class PPopup extends React.Component {
             <label>Late Sleeper?: </label>
             <Input s={12} id='latesleeper' className="inputs" type='select' name='latesleep' value={this.state.latesleep}
               onChange={this.handleChange.bind(this)} >
+              <option value='0'>No Pref</option> 
               <option value='-1'>No</option>
               <option value='1'>Yes</option>
             </Input>
             <label>Deep Sleeper?: </label>
             <Input s={12} id='deepsleeper' className="inputs" type='select' name='deepsleep' value={this.state.deepsleep} 
               onChange={this.handleChange.bind(this)}>
+              <option value='0'>No Pref</option> 
               <option value='-1'>No</option>
               <option value='1'>Yes</option>
             </Input>
             <label>Early Riser?: </label>
             <Input s={12} id='earlyriser' className="inputs" type='select' name='earlyrise' value={this.state.earlyrise}
               onChange={this.handleChange.bind(this)}>
+              <option value='0'>No Pref</option> 
               <option value='-1'>No</option>
               <option value='1'>Yes</option>
             </Input>
@@ -126,6 +130,7 @@ class PPopup extends React.Component {
             <label>How Messy Are You?: </label>
             <Input s={12} id='messyman' className="inputs" type='select' name='messy' value={this.state.messy}
               onChange={this.handleChange.bind(this)}>
+              <option value='0'>No Pref</option> 
               <option value='1'>Not at All</option>
               <option value='2'>A Little</option>
               <option value='3'>Moderately</option>
@@ -135,6 +140,7 @@ class PPopup extends React.Component {
             <label>Video Games?: </label>
             <Input s={12} id='gamer' className="inputs" type='select' name='videogames' value={this.state.videogames}
               onChange={this.handleChange.bind(this)}>
+              <option value='0'>No Pref</option> 
               <option value='-1'>Never</option>
               <option value='1'>Rarely</option>
               <option value='2'>Occasionally</option>
@@ -143,6 +149,7 @@ class PPopup extends React.Component {
             <label>Extroverted?: </label>
             <Input s={12} id='social' className="inputs" type='select' name='extro' value={this.state.extro}
               onChange={this.handleChange.bind(this)}>
+              <option value='0'>No Pref</option> 
               <option value='1'>Not at All</option>
               <option value='2'>A Little</option>
               <option value='3'>Moderately</option>
@@ -159,6 +166,7 @@ class PPopup extends React.Component {
             <label>Study Noise Level?: </label>
             <Input s={12} id='studynoise' className="inputs" type='select' name='noise' value={this.state.noise}
               onChange={this.handleChange.bind(this)}>
+              <option value='0'>No Pref</option> 
               <option value='1'>Very Quiet</option>
               <option value='2'>Quiet</option>
               <option value='3'>Moderate</option>
@@ -168,6 +176,7 @@ class PPopup extends React.Component {
             <label>Are you willing to share belongings?: </label>
             <Input s={12} id='belongings' className="inputs" type='select' name="share" value={this.state.share}
               onChange={this.handleChange.bind(this)}>
+              <option value='0'>No Pref</option> 
               <option value='1'>Not at All</option>
               <option value='2'>A Little</option>
               <option value='3'>Moderately</option>
@@ -177,6 +186,7 @@ class PPopup extends React.Component {
             <label>What relationship do you expect from a roommate? </label>
             <Input id='friends' className="inputs" type='select' name="relations" value={this.state.relations}
               onChange={this.handleChange.bind(this)}>
+              <option value='0'>No Pref</option> 
               <option value='1'>Find Friends Elsewhere</option>
               <option value='2'>Get Along</option>
               <option value='3'>Close Friends</option>
