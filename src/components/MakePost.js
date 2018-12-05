af9063b39e226db2bf99c13fc614cca8cd0518c2
tesriram <43828762+tesriram@users.noreply.github.com>
2018-12-04 17:25:26 -0800

    restarting makePost modal from scratch

--- src/components/MakePost.js
+++ src/components/MakePost.js
@@ -4,28 +4,38 @@ import {Input, Row} from 'react-materialize';
 
 export default class MakePost extends Component {
 
-     constructor(props){
-         super(props);
-         this.state = {
-             roomType: 'single',
-             homeType: 'house',
-             
-             address: '',
-             price: 0,
-             addNotes: ''
-         };
+    constructor(props){
+        super(props);
+        this.state = {
+            user_id_posted: "",
 
-         this.submitPost = this.submitPost.bind(this);
-     }
+            title: "",
+            address: "",
+            description: "",
+            dist_to_campus: "",
+            price: 0,
+            home_type: 'house',
+            room_type: 'single',
+            
+            pet: "-1",
+            in_unit_laundry: "-1",
+            furnished: "-1",
+            gym: "-1",
+            pool: "-1",
+            parking: "-1"          
+        };
 
-     handleChange(e) {
-         let target = e.target;
-         let value = target.type === 'checkbox' ? target.checked : target.value;
-         let name = target.name;
+        this.submitPost = this.submitPost.bind(this);
+    }
 
-         this.setState({
-           [name]: value
-         });
+    handleChange(e) {
+        let target = e.target;
+        let value = target.type === 'checkbox' ? target.checked : target.value;
+        let name = target.name;
+
+        this.setState({
+          [name]: value
+        });
     }
 
     submitPost(e) {
@@ -40,43 +50,32 @@ export default class MakePost extends Component {
         return <div className="postModal">
             <div className="modal">
                 <h3 id="makePostTitle">Make a Post</h3>
-                <div id="postFormInputs">
-                    <form onSubmit={this.submitPost} className="PostForms">
-                        <Row className="col-sm-6">
-                            <Input className="browser-default" id="roomType" type ='select' label="Room Type" name="roomType" defaultValue = {this.state.roomType} onChange={this.handleChange.bind(this)}>
-                                <option value="single">Single</option>
-                                <option value="double">Double</option>
-                                <option value="triple">Triple</option>
-                                <option value="livingRoom">Living Room</option>
-                            </Input>
+                <div className="container postContainer">
+                    {/* <form onSubmit={this.submitPost}> */}
+                        <div className="row">
+                            <div className="col-sm-8 infoBox">
 
-                            <div className="mpInputDiv">
-                                <label className="MPlabel" htmlFor="MPaddress">Address</label>
-                                <input id="MPaddress" className="MPinput" placeholder="Enter the address" name="address" value={this.state.address} onChange={this.handleChange.bind(this)} />
                             </div>
+                            <div className="col-sm-4 featureBox">
 
-                            <div class=".input-field" >
-                                <label className="MPlabel" htmlFor="addNotes" id="mytestingniqqa">Additional Notes</label>
-                                <Input type="textarea" id="addNotes" className="MPinput" placeholder="Write any important details here" name="addNotes" value={this.state.addNotes} onChange={this.handleChange.bind(this)}></Input>
                             </div>
-                        </Row>
-                        <Row className="col-sm-6">
-                            <Input id="homeType" className="browser-default" id="homeType" type ='select' label="Home Type" name="homeType" defaultValue = {this.state.homeType} onChange={this.handleChange.bind(this)}>
-                                <option value="house">House</option>
-                                <option value="apartment">Apartment</option>
-                            </Input>
+                        </div>
 
-                            <div className="mpInputDiv">
-                                <label className="MPlabel" htmlFor="MPprice">Price/Month</label>
-                                <input id="MPprice" className="MPinput" placeholder="Enter the monthly price" name="price" value={this.state.price} onChange={this.handleChange.bind(this)} />
+                        <div className="row">
+                            <div className = "col-sm-12 picBox">
+
                             </div>
-                            <div id="picUpload">
-                                <Input  type="file" label="Pictures" s={12} />
-                            </div>
-                        </Row>
-                        <button type="Submit" id="submitPost">Post Listing</button>      
-                    </form>
-                    <button id="cancelPost" onClick={this.cancelPost}>Cancel</button>
+                        </div>
+                        <button type="Submit" id="submitPost">Post Listing</button>
+                    {/* </form> */}
+
+                    {/* <div id="postFormInputs">
+                        <form onSubmit={this.submitPost} className="PostForms">
+                            
+                            <button type="Submit" id="submitPost">Post Listing</button>      
+                        </form>
+                    </div> */}
+
                 </div>
             </div>
         </div>
