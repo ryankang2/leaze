4b1fe1939eb2bba035bb4b623c821428b8ff5315
Ryan Kang <shk130@ucsd.edu>
2018-12-05 21:03:34 -0800

    sha512

--- public/api/queries/sign_in.php
+++ public/api/queries/sign_in.php
@@ -10,8 +10,7 @@
 
     //need to sha1 password
     $email = $_POST["email"];
-    $password = hash("sha512", $_POST["password"]);
-    // $password = $_POST["password"];
+    $password = sha512($_POST["password"]);
 
     $checkUserQuery = "SELECT * from `users` WHERE `email`='$email'";
     $result = mysqli_query($conn, $checkUserQuery);
