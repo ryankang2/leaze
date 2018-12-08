3ac443316a7c6d8c880c7c7020acd1030049f8ea
Ryan Kang <shk130@ucsd.edu>
2018-12-07 18:05:33 -0800

    eric will finish php file

--- public/api/queries/searchbox.php
+++ public/api/queries/searchbox.php
@@ -5,7 +5,13 @@
     $output = [
         "success" => false,
     ];
+
+    $input = $_POST["searchQuery"].explode(" ");
+    $length = count($input);
     
+    for($i = 0; $i < $length; $i++){
+
+    }
 
     print_r(json_encode($output));
 ?>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,6 +43,7 @@ class Navbar extends Component{
     logout(user){
         sessionStorage.setItem("user_id", "-1");
     }
+    
     async handleFormSubmit(event){
         event.preventDefault();
         event.stopPropagation();
@@ -75,39 +76,29 @@ class Navbar extends Component{
                                     <img className="logoPic" src={require('../../dist/logo_transparent.png')} alt=""/>
                                 </div>              
                             </Link>
-                            {/*<form className="col-xs-3 col-sm-5 col-md-5 navbar-form navbar-left" onSubmit={(event) => this.handleFormSubmit(event)}>*/}
-                                {/*<div className="form-group">*/}
-                                    {/*<input type="search " value={searchQuery} name="userInput" className="form-control searchBox" placeholder="Search..." onChange={(event) => this.handleInput(event)}/>*/}
-                                    {/*<i className="searchIcon fa fa-search"></i>*/}
-                                {/*</div>*/}
-                            {/*</form>*/}
-                            <ul className="col-xs-2 col-sm-2 col-md-2 profileContainer">
+                            <form className="col-xs-3 col-sm-5 col-md-5 navbar-form navbar-left" onSubmit={(event) => this.handleFormSubmit(event)}>
+                                <div className="form-group">
+                                    <input type="search " value={searchQuery} name="userInput" className="form-control searchBox" placeholder="Search..." onChange={(event) => this.handleInput(event)}/>
+                                    <i className="searchIcon fa fa-search"></i>
+                                </div>
+                            </form>
+                            <div className="col-xs-2 col-sm-2 col-md-2 profileContainer">
                                 <Link to = {linkQuery}>
                                     <div className="welcome">
                                         <i className="fa fa-user-circle-o"></i>
                                     </div>
                                 </Link>
-
-                            </ul>
-                            <ul className="col-xs-2 col-sm-2 col-md-2 settingsContainer">
-                                <ul className="nav navbar-nav editContainer">
-                                    <li onClick={this.openModal.bind(this)}><a href="#"><i className="glyphicon glyphicon-edit"></i></a></li>
-                                </ul>
-
-                                <Link to = {linkQueryMessages}>
-                                    <ul className="nav navbar-nav envelopeContainer">
-                                        <li><a href="#"><i className="glyphicon glyphicon-envelope"></i></a></li>
-                                    </ul>
-                                </Link>
-
-                                <Link to = {linkQueryLogin}>
-                                    <ul className="nav navbar-nav logoutContainer">
-                                        <li><a href="#"><i className="glyphicon glyphicon-off" onClick={this.logout.bind(this)}></i></a></li>
-                                    </ul>
-                                </Link>
-
-                                    {/*<li><a href="#"><i className="glyphicon glyphicon-bell"></i></a></li>*/}
-                            </ul>
+                                <div className="settingsContainer">
+                                    <div className="nav navbar-nav editContainer">
+                                        <li onClick={this.openModal.bind(this)}><a href="#"><i className="glyphicon glyphicon-edit"></i></a></li>
+                                    </div>
+                                    <Link to = {linkQueryLogin}>
+                                        <div className="nav navbar-nav logoutContainer">
+                                            <li><a href="#"><i className="glyphicon glyphicon-off" onClick={this.logout.bind(this)}></i></a></li>
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <MakePostModal>
