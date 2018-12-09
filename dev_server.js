64ff8ee13d93b2a57a1890299285c38a83bdb411
Ryan Kang <shk130@ucsd.edu>
2018-12-09 08:30:55 -0800

    public

--- dev_server.js
+++ dev_server.js
@@ -6,7 +6,7 @@ const npm_config = require('./package.json');
 const PORT = process.env.PORT || 3000;
 
 new WebpackDevServer(webpack(config), {
-    contentBase: resolve(__dirname, 'dist'),
+    contentBase: resolve(__dirname, 'public'),
     publicPath: '/dist/',
     hot: false,
     historyApiFallback: true,
